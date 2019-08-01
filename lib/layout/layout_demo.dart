9f94b7b7157179191f845a9bd650bfaf9655e1c1
luoxiaoke <wtimexiaoke@gmail.com>
2019-08-01 10:55:19 +0800

    Update layout_demo.dart

--- lib/layout/layout_demo.dart
+++ lib/layout/layout_demo.dart
@@ -16,7 +16,7 @@ void main() {
 
 class Body extends StatelessWidget {
   Widget titleSection = Container(
-    padding: const EdgeInsets.all(16),
+    padding: const EdgeInsets.all(32),
     child: Row(
       children: [
         Expanded(
@@ -89,24 +89,23 @@ class Body extends StatelessWidget {
     ),
   );
 
-  Widget textSection = Expanded(
-      child: Container(
-    padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
     child: Text(
       'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-      'Alps. Situated 1,578 meters above sea level, it is one of the '
-      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-      'half-hour walk through pastures and pine forest, leads you to the '
-      'lake, which warms to 20 degrees Celsius in the summer. Activities '
-      'enjoyed here include rowing, and riding the summer toboggan run.',
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
       softWrap: true,
     ),
-  ));
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
+    return ListView(
+      children: [
         Image.asset(
           'res/images/layout_example_img.jpg',
           width: 600,
