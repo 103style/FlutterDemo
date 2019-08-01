3e1d1fcc33dadc660cf2e74fbc60797d981d8236
luoxiaoke <wtimexiaoke@gmail.com>
2019-08-01 14:19:43 +0800

    Create parent_manges_state.dart

---
+++ lib/actions/parent_manges_state.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var title = 'parent manages widget’s  state';
+
+  runApp(MaterialApp(
+    title: title,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ParentWidget(),
+    ),
+  ));
+}
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapBoxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
