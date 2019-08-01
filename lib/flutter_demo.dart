a9255ca9d1fcb91a0ea9e5d0b1f4573e0fd89b45
xiaoke.luo <xiaoke.luo@tcl.com>
2019-08-01 17:13:18 +0800

    update

---
+++ lib/flutter_demo.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'actions/action_demo.dart';
+import 'common/common_utils.dart';
+import 'fisrt_demo.dart';
+import 'hello_world.dart';
+import 'layout/layout_demo.dart';
+import 'layout/layout_demo_with_action.dart';
+import 'widgets/widgets_demo.dart';
+
+var title = 'Flutter Demo';
+
+void main() {
+  runApp(FlutterDemo());
+}
+
+class FlutterDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CommonTitle(
+      title: title,
+      bodyWidget: Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  Widget _navigator(BuildContext context, String title, Widget widget) {
+    return GestureDetector(
+      child: Container(
+        margin: const EdgeInsets.only(left: 8.0, right: 8.0, top: 4.0),
+        child: MaterialButton(
+          color: Colors.lightBlue,
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => widget),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        _navigator(context, 'Hello World', HelloWorld()),
+        _navigator(context, 'First Demo', FirstDemo()),
+        _navigator(context, 'Action Demo', ActionDemo()),
+        _navigator(context, 'Widgets Demo', WidgetsDemo()),
+        _navigator(context, 'layout demo', LayoutDemo()),
+        _navigator(context, 'layout demo with action', LayoutDemoWithAction()),
+      ],
+    );
+  }
+}
