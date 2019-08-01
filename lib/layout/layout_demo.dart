88f25c8cd48c1ae8b8f1ad1ad2242672eb4530ae
luoxiaoke <wtimexiaoke@gmail.com>
2019-08-01 11:01:09 +0800

    Update layout_demo.dart

--- lib/layout/layout_demo.dart
+++ lib/layout/layout_demo.dart
@@ -45,7 +45,10 @@ class Body extends StatelessWidget {
           Icons.star,
           color: Colors.red[500],
         ),
-        Text('41'),
+        Container(
+          margin: const EdgeInsets.only(left: 4),
+          child: Text('41'),
+        ),
       ],
     ),
   );
@@ -93,11 +96,11 @@ class Body extends StatelessWidget {
     padding: const EdgeInsets.all(32),
     child: Text(
       'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
+      'Alps. Situated 1,578 meters above sea level, it is one of the '
+      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+      'half-hour walk through pastures and pine forest, leads you to the '
+      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+      'enjoyed here include rowing, and riding the summer toboggan run.',
       softWrap: true,
     ),
   );
